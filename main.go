ab0fb46ba41510218ef698d4d10d07aceaffad4d
Lucas Leão <heylucasleao@gmail.com>
2023-10-08 21:27:23 -0400

    ♻️ adjust NewColumnValues to work properly

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	config "zhang/configuration"
 	pipe "zhang/pipeline"
 )
@@ -9,6 +10,8 @@ func main() {
 	//columns := config.SparseColumns()
 	f := config.NewReadNpz()
 	shape, indptr, indices, data := config.NewReadNpy(f)
-	csc_matrix := pipe.NewCSCMatrix(shape[0], shape[1], indptr, indices, data)
-	pipe.NewColumnValues(csc_matrix)
+	csr_matrix := pipe.NewCSRMatrix(shape[0], shape[1], indptr, indices, data)
+	csc_matrix := csr_matrix.ToCSC()
+	arr := pipe.NewColumnValues(csc_matrix, 0)
+	fmt.Println(arr)
 }
--- pipeline/matrix.go
+++ pipeline/matrix.go
@@ -4,17 +4,16 @@ import (
 	"github.com/james-bowman/sparse"
 )
 
-func NewCSCMatrix(r int, c int, indptr []int, indices []int, data []float64) *sparse.CSC {
-	return sparse.NewCSC(r, c, indptr, indices, data)
+func NewCSRMatrix(r int, c int, indptr []int, indices []int, data []float64) *sparse.CSR {
+	return sparse.NewCSR(r, c, indptr, indices, data)
 }
 
-func NewColumnValues(m *sparse.CSC) *[]float64 {
-	arr := make([]float64, m.RawMatrix().J)
-	for i := 0; i < m.RawMatrix().J; i++ {
-		sl := m.RawMatrix().Ind[m.RawMatrix().Indptr[i]:m.RawMatrix().Indptr[i+1]]
-		for _, j := range sl {
-			arr[j] = 1
-		}
+func NewColumnValues(mat *sparse.CSC, idx int) *[]float64 {
+	arr := make([]float64, mat.RawMatrix().J)
+	sl := mat.RawMatrix().Ind[mat.RawMatrix().Indptr[idx]:mat.RawMatrix().Indptr[idx+1]]
+
+	for _, j := range sl {
+		arr[j] = 1
 	}
 	return &arr
 }
